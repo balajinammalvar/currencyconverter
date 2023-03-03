10b295b96276583250a61eadad1311f437b0c0fe
balajinammalvar <balajianb@gmail.com>
2023-03-03 20:02:53 +0530

    * Xerox

---
+++ app/src/main/java/online/balaji/currencyconverter/XeroxActivity.kt
@@ -0,0 +1,11 @@
+package online.balaji.currencyconverter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class XeroxActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_xerox)
+    }
+}
